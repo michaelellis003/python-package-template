00b7757fc723659ae4448c937220e5611f5317ad
Michael Ellis <michaelellis003@gmail.com>
2025-01-25 13:51:32 -0600

    change package import

--- src/python_package_template/__init__.py
+++ src/python_package_template/__init__.py
@@ -1,42 +1,5 @@
-"""This is the python_package_template module.
+"""This is the python_package_template package."""
 
-It provides basic functions for demonstration purposes.
-"""
+from .main import add, hello, multiply  # re-export
 
-
-def hello(name: str = "world") -> str:
-    """Return a greeting string.
-
-    Args:
-        name (str): The name to greet.
-
-    Returns:
-        str: The greeting string.
-    """
-    return f"Hello {name}"
-
-
-def add(a: int, b: int) -> int:
-    """Add two integers.
-
-    Args:
-        a (int): The first integer.
-        b (int): The second integer.
-
-    Returns:
-        int: The sum of the two integers.
-    """
-    return a + b
-
-
-def multiply(a: float, b: int) -> float:
-    """Multiply a float by an integer.
-
-    Args:
-        a (float): The float number.
-        b (int): The integer number.
-
-    Returns:
-        float: The product of the float and the integer.
-    """
-    return a * b
+__all__ = ["add", "multiply", "hello"]
